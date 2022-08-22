3d229548cdb9e68674721b78c2ea6c7ef3e691e8
MHRafix <fmhrafiz27@gmail.com>
2022-08-22 12:26:57 +0600

    fixing:-

--- src/pages/admin_pannel/[admin_email]/manage_products/all_products/index.js
+++ src/pages/admin_pannel/[admin_email]/manage_products/all_products/index.js
@@ -1,72 +1,72 @@
-// import React, { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import AdminPannelLayoutContainer from "../../../../../components/admin_pannel_components/common/layout/AdminPannelLayoutContainer";
-// import AllProductsMain from "../../../../../components/admin_pannel_components/components/manage_products/all_products/AllProductsMain";
-// import { storeAllCategories } from "../../../../../redux/all_data/action";
-// import { storeUserData } from "../../../../../redux/user_data/action";
-// import ErrorPage from "../../../../404";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import AdminPannelLayoutContainer from "../../../../../components/admin_pannel_components/common/layout/AdminPannelLayoutContainer";
+import AllProductsMain from "../../../../../components/admin_pannel_components/components/manage_products/all_products/AllProductsMain";
+import { storeAllCategories } from "../../../../../redux/all_data/action";
+import { storeUserData } from "../../../../../redux/user_data/action";
+import ErrorPage from "../../../../404";
 
-// export default function AllProductsPage({
-//   all_products,
-//   all_categories,
-//   this_user,
-// }) {
-//   // store categories to redux
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(storeUserData(this_user));
-//     dispatch(storeAllCategories(all_categories));
-//   });
+export default function AllProductsPage({
+  all_products,
+  all_categories,
+  this_user,
+}) {
+  // store categories to redux
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(storeUserData(this_user));
+    dispatch(storeAllCategories(all_categories));
+  });
 
-//   if (!this_user?.user_admin) {
-//     return <ErrorPage />;
-//   }
-//   return (
-//     <>
-//       <AdminPannelLayoutContainer
-//         title="Manage Products"
-//         description="This is manage products of 'Daily Needs Grocery' web application admin pannel."
-//       >
-//         <AllProductsMain
-//           all_products={all_products}
-//           all_categories={all_categories}
-//         />
-//       </AdminPannelLayoutContainer>
-//     </>
-//   );
-// }
-
-// // find the right user here
-// export async function getStaticPaths() {
-//   const users = await fetch("https://daily-need.vercel.app/api/all_users");
-//   const all_users = await users.json();
-//   const user = all_users.map((user) => ({
-//     params: { admin_email: user.user_email },
-//   }));
-//   return {
-//     paths: user,
-//     fallback: false,
-//   };
-// }
-
-// // return the data here
-// export async function getStaticProps({ params }) {
-//   const { admin_email } = params;
-//   const user = await fetch(
-//     `https://daily-need.vercel.app/api/admin_pannel_api/manage_users/${admin_email}`
-//   );
-//   const this_user = await user.json();
-
-//   // all products
-//   const products = await fetch(`${process.env.ROOT_URI}/api/allproducts`);
-//   const categories = await fetch(`${process.env.ROOT_URI}/api/allcategories`);
-//   const all_products = await products.json();
-//   const all_categories = await categories.json();
-
-//   return { props: { all_products, all_categories, this_user }, revalidate: 10 };
-// }
-import React from "react";
-
-export default function Allproducts() {
-  return <div>allproducts</div>;
+  if (!this_user?.user_admin) {
+    return <ErrorPage />;
+  }
+  return (
+    <>
+      <AdminPannelLayoutContainer
+        title="Manage Products"
+        description="This is manage products of 'Daily Needs Grocery' web application admin pannel."
+      >
+        <AllProductsMain
+          all_products={all_products}
+          all_categories={all_categories}
+        />
+      </AdminPannelLayoutContainer>
+    </>
+  );
 }
+
+// find the right user here
+export async function getStaticPaths() {
+  const users = await fetch("https://daily-need.vercel.app/api/all_users");
+  const all_users = await users.json();
+  const user = all_users.map((user) => ({
+    params: { admin_email: user.user_email },
+  }));
+  return {
+    paths: user,
+    fallback: false,
+  };
+}
+
+// return the data here
+export async function getStaticProps({ params }) {
+  const { admin_email } = params;
+  const user = await fetch(
+    `https://daily-need.vercel.app/api/admin_pannel_api/manage_users/${admin_email}`
+  );
+  const this_user = await user.json();
+
+  // all products
+  const products = await fetch(`${process.env.ROOT_URI}/api/allproducts`);
+  const categories = await fetch(`${process.env.ROOT_URI}/api/allcategories`);
+  const all_products = await products.json();
+  const all_categories = await categories.json();
+
+  return { props: { all_products, all_categories, this_user }, revalidate: 10 };
+}
+// import React from "react";
+
+// export default function Allproducts() {
+//   return <div>allproducts</div>;
+// }
