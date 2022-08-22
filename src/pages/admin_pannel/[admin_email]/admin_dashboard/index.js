3c5542ab3588d50ab4c43d9f99396cee6e1bf82f
MHRafix <fmhrafiz27@gmail.com>
2022-08-22 11:01:47 +0600

    fixing

--- src/pages/admin_pannel/[admin_email]/admin_dashboard/index.js
+++ src/pages/admin_pannel/[admin_email]/admin_dashboard/index.js
@@ -33,42 +33,42 @@ export default function AdminDashboard({
 }
 
 // find the user
-export async function getStaticPaths() {
-  const users = await fetch("https://daily-need.vercel.app/api/all_users");
-  const all_users = await users.json();
-  const user = all_users.map((user) => ({
-    params: { admin_email: user.user_email },
-  }));
-  return {
-    paths: user,
-    fallback: false,
-  };
-}
+// export async function getStaticPaths() {
+//   const users = await fetch("https://daily-need.vercel.app/api/all_users");
+//   const all_users = await users.json();
+//   const user = all_users.map((user) => ({
+//     params: { admin_email: user.user_email },
+//   }));
+//   return {
+//     paths: user,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const { admin_email } = params;
-  const user = await fetch(
-    `https://daily-need.vercel.app/api/admin_pannel_api/manage_users/${admin_email}`
-    // `http://localhost:3000/api/admin_pannel_api/manage_users/${admin_email}`
-  );
-  const this_user = await user.json();
+// export async function getStaticProps({ params }) {
+//   const { admin_email } = params;
+//   const user = await fetch(
+//     `https://daily-need.vercel.app/api/admin_pannel_api/manage_users/${admin_email}`
+//     // `http://localhost:3000/api/admin_pannel_api/manage_users/${admin_email}`
+//   );
+//   const this_user = await user.json();
 
-  // all orders
-  const orders = await fetch(
-    `https://daily-need.vercel.app/api/manage_orders/all_orders`
-  );
-  const all_orders = await orders.json();
+//   // all orders
+//   const orders = await fetch(
+//     `https://daily-need.vercel.app/api/manage_orders/all_orders`
+//   );
+//   const all_orders = await orders.json();
 
-  // all products
-  const products = await fetch(`https://daily-need.vercel.app/api/allproducts`);
-  const all_products = await products.json();
+//   // all products
+//   const products = await fetch(`https://daily-need.vercel.app/api/allproducts`);
+//   const all_products = await products.json();
 
-  // all users
-  const users = await fetch(`https://daily-need.vercel.app/api/all_users`);
-  const all_users = await users.json();
+//   // all users
+//   const users = await fetch(`https://daily-need.vercel.app/api/all_users`);
+//   const all_users = await users.json();
 
-  return {
-    props: { all_orders, all_products, all_users, this_user },
-    revalidate: 10,
-  };
-}
+//   return {
+//     props: { all_orders, all_products, all_users, this_user },
+//     revalidate: 10,
+//   };
+// }
