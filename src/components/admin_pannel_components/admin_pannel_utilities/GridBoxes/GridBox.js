2718099019c88776981e0338a4398569a0269dbb
MHRafix <fmhrafiz27@gmail.com>
2022-08-25 07:15:35 +0600

    motion add to card of framer motion

--- src/components/admin_pannel_components/admin_pannel_utilities/GridBoxes/GridBox.js
+++ src/components/admin_pannel_components/admin_pannel_utilities/GridBoxes/GridBox.js
@@ -1,36 +1,47 @@
-import React from "react";
-import CountUp from "react-countup";
+import { motion } from 'framer-motion';
+import React from 'react';
+import CountUp from 'react-countup';
+import cardAnimation from '../../../../hooks/animation/cardAnimation';
 
 export default function GridBox({ box_content }) {
-  const { box_name, box_number, box_icon, icon_color, note } = box_content;
-  return (
-    <>
-      <div id="box_wrapper">
-        <div className="box_content">
-          <div>
-            <span id="chart_indicator_label">{box_name}</span>
-          </div>
-          <div>
-            <div id="amount_label" style={{ height: "43px" }}>
-              <CountUp
-                start={box_number > 1000 ? box_number - 500 : 0}
-                end={box_number}
-                duration={2}
-                separator=","
-              />
-            </div>
-          </div>
-          {note && (
-            <p className="mt-2 text-thin text-black3 tracking-wider">{note}</p>
-          )}
-        </div>
-        <div
-          className="box_icon"
-          style={{ fontSize: "50px", color: `${icon_color}` }}
-        >
-          {box_icon}
-        </div>
-      </div>
-    </>
-  );
+	const { box_name, box_number, box_icon, icon_color, note } = box_content;
+	const { initial, animate, exit, transition } = cardAnimation();
+	return (
+		<>
+			<motion.div
+				id='box_wrapper'
+				whileInView='onscreen'
+				viewport={{ once: true, amount: 0.8 }}
+				initial={initial}
+				animate={animate}
+				exit={exit}
+				transition={transition}
+			>
+				<div className='box_content'>
+					<div>
+						<span id='chart_indicator_label'>{box_name}</span>
+					</div>
+					<div>
+						<div id='amount_label' style={{ height: '43px' }}>
+							<CountUp
+								start={box_number > 1000 ? box_number - 500 : 0}
+								end={box_number}
+								duration={2}
+								separator=','
+							/>
+						</div>
+					</div>
+					{note && (
+						<p className='mt-2 text-thin text-black3 tracking-wider'>{note}</p>
+					)}
+				</div>
+				<div
+					className='box_icon'
+					style={{ fontSize: '50px', color: `${icon_color}` }}
+				>
+					{box_icon}
+				</div>
+			</motion.div>
+		</>
+	);
 }
--- src/fake_data/all_fakedata.js
+++ src/fake_data/all_fakedata.js
@@ -1,75 +1,75 @@
 // category image import here
-import { BsClockHistory } from "react-icons/bs";
-import { FiEdit } from "react-icons/fi";
-import { MdManageSearch, MdOutlineDashboard } from "react-icons/md";
+import { BsClockHistory } from 'react-icons/bs';
+import { FiEdit } from 'react-icons/fi';
+import { MdManageSearch, MdOutlineDashboard } from 'react-icons/md';
 
 // header navigation menus fake data here
 export const header_navigation = [
-  { _id: 1, menu_name: "home", href: "/" },
-  { _id: 2, menu_name: "about us", href: "/about_us" },
-  {
-    _id: 3,
-    menu_name: "fruits & vegetables",
-    href: "/categories/fruits_and_vegetables",
-  },
-  { _id: 4, menu_name: "shop", href: "/shop/grid_shop" },
-  { _id: 5, menu_name: "blog", href: "/blog" },
-  { _id: 6, menu_name: "FAQ", href: "/faq" },
-  { _id: 7, menu_name: "contact", href: "/contact" },
+	{ _id: 1, menu_name: 'home', href: '/' },
+	{ _id: 2, menu_name: 'about us', href: '/about_us' },
+	{
+		_id: 3,
+		menu_name: 'fruits & vegetables',
+		href: '/categories/fruits_and_vegetables',
+	},
+	{ _id: 4, menu_name: 'shop', href: '/shop/grid_shop' },
+	{ _id: 5, menu_name: 'blog', href: '/blog' },
+	{ _id: 6, menu_name: 'FAQ', href: '/faq' },
+	{ _id: 7, menu_name: 'contact', href: '/contact' },
 ];
 
 // header navigation menus fake data here
 export const profile_navigation = [
-  {
-    _id: 1,
-    menu_name: "dashboard",
-    href: "/dashboard",
-    menu_icon: <MdOutlineDashboard />,
-  },
-  {
-    _id: 2,
-    menu_name: "my all orders",
-    href: "/my_all_orders",
-    menu_icon: <MdManageSearch />,
-  },
-  {
-    _id: 3,
-    menu_name: "history download",
-    href: "/history_download",
-    menu_icon: <BsClockHistory />,
-  },
-  {
-    _id: 4,
-    menu_name: "edit details",
-    href: "/verify_user",
-    menu_icon: <FiEdit />,
-  },
+	{
+		_id: 1,
+		menu_name: 'dashboard',
+		href: '/dashboard',
+		menu_icon: <MdOutlineDashboard />,
+	},
+	{
+		_id: 2,
+		menu_name: 'my all orders',
+		href: '/my_all_orders',
+		menu_icon: <MdManageSearch />,
+	},
+	{
+		_id: 3,
+		menu_name: 'history download',
+		href: '/history_download',
+		menu_icon: <BsClockHistory />,
+	},
+	{
+		_id: 4,
+		menu_name: 'edit details',
+		href: '/verify_user',
+		menu_icon: <FiEdit />,
+	},
 ];
 
 // stock data here
 export const stock_data = [
-  { _id: 1, filter_name: "in-stock" },
-  { _id: 2, filter_name: "stock-out" },
+	{ _id: 1, filter_name: 'in-stock' },
+	{ _id: 2, filter_name: 'stock-out' },
 ];
 
 // type data here
 export const type_data = [
-  { _id: 2, filter_name: "on-sale" },
-  { _id: 3, filter_name: "fixed-sale" },
-  { _id: 4, filter_name: "offer-sale" },
+	{ _id: 2, filter_name: 'on-sale' },
+	{ _id: 3, filter_name: 'fixed-sale' },
+	{ _id: 4, filter_name: 'offer-sale' },
 ];
 
 export const month_name = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec',
 ];
