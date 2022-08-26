fee8b6964b7dd8a02cf8676b272eab40ebe9cbe6
MHRafix <fmhrafiz27@gmail.com>
2022-08-26 12:26:21 +0600

    transition fixed

--- src/components/commons/layout/LayoutContainer.js
+++ src/components/commons/layout/LayoutContainer.js
@@ -11,9 +11,9 @@ export default function LayoutContainer({ children, title, description }) {
 	return (
 		<motion.div
 			className='page_main_wrapper'
-			initial={{ scaleY: 0 }}
-			animate={{ scaleY: 1 }}
-			exit={{ scaleY: 0 }}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
 			transition={{ duration: 0.3 }}
 		>
 			<Head>
